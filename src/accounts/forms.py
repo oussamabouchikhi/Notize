2c2eda42f02de73079bfe95e9d87eb18762a766b
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-03-07 17:12:48 +0100

    edit user profile

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -6,7 +6,7 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
-        
+
 
 class ProfileForm(forms.ModelForm):
     class Meta:
